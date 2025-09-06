519ddad7fa8e10a36d2b832d7f87692c9240fb87
CHO <imalex12@naver.com>
2025-09-06 17:04:06 +0900

    Init Spring Boot project

---
+++ src/main/java/com/example/nextvalue/NextValueApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextValueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextValueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nextvalue/NextValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextValueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
