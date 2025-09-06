0dd339406e4999e0c698fcfc706b63fba7b7ca5e
Jun Yeong Kim <junyeonggim5@gmail.com>
2025-09-06 20:43:43 +0900

    Oauth google login added
    Social login through google.
    
    Signed-off-by: Jun Yeong Kim <junyeonggim5@gmail.com>

--- src/main/java/com/example/nextvalue/config/SecurityConfig.java
+++ src/main/java/com/example/nextvalue/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.nextvalue.config;
 
 import com.example.nextvalue.jwt.JwtFilter;
 import com.example.nextvalue.jwt.JwtTokenUtil;
+import com.example.nextvalue.service.Oauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -15,7 +17,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final Oauth2UserService customOAuth2UserService;
 
     // 허용할 URL을 배열의 형태로 관리
     private final String[] allowedUrls = {
@@ -23,7 +28,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/swagger-resources/**",
             "/v3/api-docs/**",
-            "/memento/**"
+            "/memento/**",
+            "*"
     };
     @Bean
     @Order(0)
@@ -44,18 +50,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenUtil jwtTokenUtil) throws Exception {
         JwtFilter jwtFilter = new JwtFilter(jwtTokenUtil);
         http
-                .securityMatcher("/**")
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(Customizer.withDefaults())//bean에서 corsConfigurationSource 찾아와서 알아서 등록
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(allowedUrls).permitAll()
-                        // 이외의 요청에 대해서는 인증이 필요하도록 설정
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//                .securityMatcher("/**")
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .formLogin(AbstractHttpConfigurer::disable)
+//                .httpBasic(httpBasic -> httpBasic.disable())
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .cors(Customizer.withDefaults())//bean에서 corsConfigurationSource 찾아와서 알아서 등록
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(allowedUrls).permitAll()
+//                        // 이외의 요청에 대해서는 인증이 필요하도록 설정
+//                        .anyRequest().authenticated())
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth -> oauth
+                .loginPage("/login")
+                .userInfoEndpoint(userInfo -> userInfo
+                        .userService(customOAuth2UserService)
+                )
+                .defaultSuccessUrl("/",true)
+        );
         return http.build();
     }
 }
